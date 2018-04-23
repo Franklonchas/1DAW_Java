2e5ccf5e4997ec9818c9b9d4d84fea6b8f83b6ac
Franklonchas <fjsancheztrabajos@gmail.com>
2018-04-23 12:39:32 +0200

    Ejercicios

--- Practica 10/src/vista.java
+++ Practica 10/src/vista.java
@@ -26,21 +26,106 @@ public class vista extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel1 = new javax.swing.JLabel();
+        combo1 = new javax.swing.JComboBox();
+        jLabel2 = new javax.swing.JLabel();
+        combo2 = new javax.swing.JComboBox();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        fecha = new javax.swing.JTextField();
+        local = new javax.swing.JTextField();
+        visitante = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        salir = new javax.swing.JButton();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(600, 500));
+        getContentPane().setLayout(null);
+
+        jLabel1.setText("Jornada");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(90, 50, 45, 16);
+
+        combo1.setModel(new javax.swing.DefaultComboBoxModel(new String[] {}));
+        combo1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                combo1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(combo1);
+        combo1.setBounds(170, 50, 70, 22);
+
+        jLabel2.setText("Partida");
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(330, 50, 40, 16);
+
+        combo2.setModel(new javax.swing.DefaultComboBoxModel(new String[] {}));
+        getContentPane().add(combo2);
+        combo2.setBounds(410, 50, 60, 22);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Datos del Partido"));
+        jPanel1.setLayout(null);
+
+        jLabel3.setText("Fecha");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(39, 46, 34, 16);
+
+        jLabel4.setText("Local");
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(33, 103, 40, 16);
+
+        jLabel5.setText("Visitante");
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(214, 103, 49, 16);
+        jPanel1.add(fecha);
+        fecha.setBounds(110, 40, 90, 22);
+        jPanel1.add(local);
+        local.setBounds(112, 100, 80, 22);
+        jPanel1.add(visitante);
+        visitante.setBounds(281, 100, 80, 22);
+
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(100, 120, 390, 160);
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(150, 300, 350, 100);
+
+        jLabel6.setText("Local");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(70, 340, 40, 16);
+
+        jLabel7.setText("Visitante");
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(70, 370, 50, 16);
+
+        salir.setText("Salir");
+        getContentPane().add(salir);
+        salir.setBounds(270, 440, 70, 25);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void combo1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combo1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_combo1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +163,21 @@ public class vista extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox combo1;
+    private javax.swing.JComboBox combo2;
+    private javax.swing.JTextField fecha;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTextField local;
+    private javax.swing.JButton salir;
+    private javax.swing.JTextField visitante;
     // End of variables declaration//GEN-END:variables
 }
